26cb5f87697c5fbdba57d5ff0a04a7bb1ad9afe7
Devin Stickells <stickells777@gmail.com>
2018-09-25 22:09:55 +0200

    Adapted prac 4 code

--- prac_6_andnic019_stcdev001.py
+++ prac_6_andnic019_stcdev001.py
@@ -11,29 +11,21 @@ GPIO.setwarnings(False)
 # init variables & constants
 GPIO.setmode(GPIO.BCM) # use GPIO pin numbering
 delay = 300 # button debounce time
-lightmax = 898 # maximum expected LDR value
 time_start = time.time() # reference time to calculate timer value
-freq_arr = [0.5,1,2] # array of delay time values in seconds
 time_delay = 0.5 # default delay is 500ms
 started = True
 stoppedcount = 0
 stoppedcache = ""
 
 # set pin names
-resetpin = 19
-freqpin = 16
-stoppin = 20
-displaypin = 21
+servicepin = 19
 CLK = 11
 MISO = 9
 MOSI = 10
 CS = 8
 
 # setup pin modes
-GPIO.setup(resetpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(freqpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(stoppin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(displaypin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(servicepin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(MOSI, GPIO.OUT)
 GPIO.setup(MISO, GPIO.IN)
 GPIO.setup(CLK, GPIO.OUT)
@@ -41,53 +33,18 @@ GPIO.setup(CS, GPIO.OUT)
 
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, mosi=MOSI, miso=MISO)
 
-# handle reset button presses
-def reset_pushed(channel):
+# handle service button presses
+def service_pushed(channel):
     global time_start
     time_start = time.time() # reset time of timer
     print("\033c", end='') #clears the console, but only when running from terminal. No way to clear IDE shell
     print("Reset pushed")
     # print header line
     print ('{:10}{:10}{:8}{:8}{:8}'.format("Time", "Timer", "Pot", "Temp", "Light"))
-GPIO.add_event_detect(resetpin, GPIO.RISING, reset_pushed, delay);
-
-# handle freq button presses
-def freq_pushed(channel):
-    global time_delay
-    index = freq_arr.index(time_delay) 
-    if (index==2): index=0  
-    else: index +=1 
-    time_delay = freq_arr[index] # iterate to next delay value
-    
-    print("Frequency pushed")
-    print("Time delay = " + str(time_delay) + "s")
-GPIO.add_event_detect(freqpin, GPIO.RISING, freq_pushed, delay);
-
-# handle stop button presses
-def stop_pushed(channel):
-    global started
-    started = not(started)
-    if (started): print("Start pushed")
-    else:  print("Stop pushed")
-    
-    # reset the stopped cache for use by the Display button
-    global stoppedcache
-    stoppedcache = ""
-    global stoppedcount
-    stoppedcount = 0
-GPIO.add_event_detect(stoppin, GPIO.RISING, stop_pushed, delay);
-
-# handle display button presses
-def display_pushed(channel):
-    global stoppedcache
-    print("Display pushed")
-    print(stoppedcache)
-GPIO.add_event_detect(displaypin, GPIO.RISING, display_pushed, delay);
+GPIO.add_event_detect(servicepin, GPIO.RISING, service_pushed, delay);
 
 def getreadings():
     # read MCP raw input values
-    tempraw = mcp.read_adc(0)
-    lightraw = mcp.read_adc(1)
     potraw = mcp.read_adc(2)
     
     # convert raw values to meaningful output numbers
@@ -96,12 +53,10 @@ def getreadings():
     
     timer = str(datetime.timedelta(seconds=timer_seconds)) # Convert seconds to correct format
     actualtime = datetime.datetime.fromtimestamp(current_ticks).strftime('%H:%M:%S') # Get the current device time
-    tempvolt = 3.3*(tempraw/1023) # convert raw value to voltage for use in formula
-    temp = (tempvolt-0.5)/(0.01)
     light = 100*(lightraw/lightmax)
     pot = 3.3*(potraw/1023)
     
-    readings = '{:10}{:6}{:6.1f} V {:4.0f} C {:4.0f}%'.format(actualtime, timer, pot, temp, light)
+    readings = '{:10}{:6}{:6.1f} V'.format(actualtime, timer, pot)
     return readings
 
 def wait():
