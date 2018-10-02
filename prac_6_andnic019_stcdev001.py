4c63429e62064a3e62c310fd968f2a913d86b18f
Devin Stickells <stickells777@gmail.com>
2018-10-03 00:13:37 +0200

    Almost working secure mode. Issues with follow-up attempts.

--- prac_6_andnic019_stcdev001.py
+++ prac_6_andnic019_stcdev001.py
@@ -11,27 +11,32 @@ GPIO.setwarnings(False)
 # init variables & constants
 GPIO.setmode(GPIO.BCM) # use GPIO pin numbering
 delay = 300 # button debounce time
-tolerance = 200 # time between pot readings
-started = True
+tolerance = 0.1 # time between pot readings
 stoppedcount = 0
 symbolstoptime = 1 # delay allowed between symbols
 codestoptime = 2 # delay considered to be the end of entering the code
 
 lastreading = 0
 goingup = True
-upwardscount = 0
-downwardscount = 0
+runcounter = 0
+waitcount = 0
 locked = True # assumed to be locked initially
+awaitingattempt = False # True when service has been pushed, but no action taken yet (for timeout)
+awaitingsymbol = False # True in above case, and when a symbol entry has timed out (after 1 second
+                        # but the next symbol hasn't started being entered yet
+sleeping = True # False when service has been pressed until the code entry timeout
 
-symbolindex = 0
-codelog = [0] * 16 # durations of symbols entered
-dirlog = [0] * 16 # directions of symbols entered
-up = 1
-down = 0
+symbolslogged = 0
+codelog = [] # durations of symbols entered
+dirlog = [] # directions of symbols entered
+right = 1
+left = 0
+dialmargin = 3 # by how much must the dial actually move to be considered intentional - for noise reduction
+timemargin = 0.5 # by how much can a symbol duration differ from what's required to be considered close enough
 
 class Combination: # hard coded lock combination
-	durations = [500, 500, 250, 250, 500, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # in milliseconds
-	directions = [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # 1 is right / up and 0 is left / down
+    durations = [1, 1, 0.5, 0.5, 1] # in seconds
+    directions = [right, left, right, left, right] 
 
 combocode = Combination()
 
@@ -59,10 +64,19 @@ mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, mosi=MOSI, miso=MISO)
 
 # handle service button presses
 def service_pushed(channel):
-    print("Service pushed")
+    print("Service pushed, awaiting combination code.")
+    
+    global sleeping
+    global awaitingattempt
+    global awaitingsymbol
+    
+    sleeping = False
+    awaitingattempt = True
+    awaitingsymbol = True
+    goingup = True
     
     # reset logs of previous attempt 
-    symbolindex = 0 
+    symbolslogged = 0 
     codelog.clear()
     dirlog.clear()
 GPIO.add_event_detect(servicepin, GPIO.RISING, service_pushed, delay);
@@ -70,87 +84,125 @@ GPIO.add_event_detect(servicepin, GPIO.RISING, service_pushed, delay);
 def getreading():
     # read MCP raw input values
     potraw = mcp.read_adc(0)
-    pot = 3.3*(potraw/1023)
+    pot = round(50*(potraw/1023)) #convert to a resolution of 50 -any finer and pot jitter becomes and issue
     
     return pot  
 
 def unlock():
-	# make a happy noise
-	# make U-Line high for 2 secs, then send low
-	GPIO.output(unlockpin, 1)
-	sleep(2)
-	GPIO.output(unlockpin, 0)
+    # make a happy noise
+    # make U-Line high for 2 secs, then send low
+    print("Unlocking")
+    GPIO.output(unlockpin, 1)
+    time.sleep(2)
+    GPIO.output(unlockpin, 0)
 
 def lock():
-	# make L-Line high for 2 secs, then send low
-	GPIO.output(lockpin, 1)
-	sleep(2)
-	GPIO.output(lockpin, 0)
+    # make L-Line high for 2 secs, then send low
+    print("Locking")
+    GPIO.output(lockpin, 1)
+    time.sleep(2)
+    GPIO.output(lockpin, 0)
 
 def unlockfail():
-	# make a sad noise
-
+    print("Combination incorrect, press service button to try again.")
+    return
+    # make a sad noise
+          
 def sort (x, y, n): # sort x into an array y of length n from min to max with ARM assembly
-	# bleh
+    return
+    # bleh
 
 def checkcombination(inputdurations, inputdirections): # takes in a combination and checks it against the preset combocode
-	correct = True
-	global combocode
+    correct = True
+    global combocode
+    global sleeping
+    global symbolslogged
+    global timemargin
 
-	for index in xrange(0,15):
-		if (combocode.directions[index] != inputdirections[index] || combocode.durations[index] != inputdurations[index])
-			correct = False
-
-	return correct
+    if (len(combocode.directions) != len(inputdirections)): # rule out attempts with wrong number of symbols
+        correct = False
+    else:
+        for i in range(len(inputdirections)): 
+            if (combocode.directions[i] != inputdirections[i] or (abs(combocode.durations[i] - inputdurations[i]) > timemargin)):
+                correct = False
+    
+    print("Checked combination")
+    sleeping = True
+    return correct
 
 def logsymbol(duration, direction):
-	codelog[symbolindex] = duration # add duration of last symbol to log
-    dirlog[symbolindex] = direction # add direction of last symbol to log
-
+    global symbolslogged
+    symbolslogged += 1
+    
+    codelog.append(duration) # add duration of last symbol to log
+    dirlog.append(direction) # add direction of last symbol to log
+    
+    if (direction == 1):
+        print("Turned right for " + str(duration) + "s")
+    else:
+        print("Turned left for " + str(duration) + "s")
+    
 #try-finally block to handle GPIO cleanup and robust termination
 try:
-    
     #loop for programme execution    
     while True: # make the code run until an exception is thrown
         global goingup
-        reading = getreading()
-        inc(runcounter)
+        global runcounter
+        global awaitingsymbol
+        global awaitingattempt
+        global sleeping
+        global waitcount
+        global dialmargin
+        
+        if not (sleeping): # foil attempts when service button hasn't been pressed
+            if not (awaitingsymbol): # only increase run counter when a symbol is actually being entered
+                runcounter += 1
+            reading = getreading()
+        else:
+            continue
 
-        if (reading == lastreading): # we've stopped (temporarily or otherwise...)
-			inc(waitcount) 
+        if ((reading <= lastreading + dialmargin) and (reading >= lastreading - dialmargin)): # we've stopped (temporarily or otherwise...) [with dialmargin]
+            waitcount += 1 
 
-			if (waitcount * tolerance >= codestoptime): # code entering completed
-				if (checkcombination(codelog, dirlog)): # check if code entered is correct
-					unlock()
-				else:
-					unlockfail()
+            if (waitcount * tolerance >= codestoptime): # code entering completed
+                waitcount = 0
+                sleeping = True # put back to sleep, awaiting service button
+                if (awaitingattempt): # user never actually entered anything in 2 seconds
+                    print("System timed out. Press service button to enter a combination")
+                elif (checkcombination(codelog, dirlog)): # check if code entered is correct
+                    unlock()
+                else:
+                    unlockfail()
+            
+            elif ((waitcount * tolerance >= symbolstoptime) and not (awaitingsymbol)): # consider current symbol to be finished 
+                #print("Symbol timed out")
+                logsymbol((runcounter-waitcount) * tolerance, goingup) # only count up to when the dial stopped moving
+                goingup = True
+                awaitingsymbol = True 
+                runcounter = 0
 
-			else if (waitcount * tolerance >= symbolstoptime): # consider current symbol to be finished 
-				logsymbol(runcounter * tolerance, goingup)
-				awaitingattempt = True 
-				runcounter = 0
+        else:
+            awaitingattempt = False # the user has done something
+            awaitingsymbol = False
+            waitcount = 0
+        
+            if (goingup): # currently in a rightwards symbol (default at start of every symbol)
+                if (reading < lastreading): # dial turned left - may be starting attempt with left turn, or ending a right turn    
+                    if not (awaitingsymbol): # this isn't a left-starting code entry, we've actually changed direction
+                        #print("Turned left to end right symbol")
+                        logsymbol(runcounter * tolerance, goingup)
+                    goingup = False
+                    runcounter = 0
+              
+            else: # goingup is false - we're in a leftwards symbol
+                if (reading > lastreading): # dial turned rightwards - end of left symbol`
+                    #print("Turned right to end left symbol")
+                    logsymbol(runcounter * tolerance, goingup)
+                    goingup = True
+                    runcounter = 0
 
-        if (goingup): # currently in a rightwards symbol
-        	if (reading >= lastreading): # stopped or still turning right
-        		awaitingattempt = False
-        	else: # direction changed
-        		if (!awaitingattempt): # this isn't a left-starting code entry, we've actually changed direction
-        			logsymbol(runcounter * tolerance, goingup)
-				
-				goingup = False
-				runcounter = 0
-
-		else: # in a leftwards symbol
-			if (reading <= lastreading): # stopped or still turning left
-				awaitingattempt = False
-			else:
-				logsymbol(runcounter * tolerance, goingup)
-				
-				goingup = True
-				runcounter = 0
-
-		lastreading = reading
-        sleep(tolerance)    
+        lastreading = reading
+        time.sleep(tolerance)    
 
 finally:
     GPIO.cleanup()
